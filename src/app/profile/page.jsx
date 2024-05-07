d6918c4abd982b744535eef430f0269b72e7b32c
SetUp <abdulmumeenibroheem51@gmail.com>
2024-05-07 13:00:02 +0100

    feat(added edit icon)

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,7 +6,10 @@ import Avatar from "@/assets/Avvatar.png";
 import userProfiles from "@/data/mockData";
 import { BasicDetails } from "@/components/user/Detailsone";
 import Maininfo from "@/components/user/mainDetails";
+import { MdModeEdit } from "react-icons/md";
+import { useState } from "react";
 const Dashboard = () => {
+  const [imgSrc, setImgsrc] = useState(Avatar);
   return (
     <Fragment>
       <div className="dashboard-wrapper">
@@ -17,7 +20,11 @@ const Dashboard = () => {
           <section className="profile_images relative w-full h-[25vh]">
             {/* avatar_pic */}
             <div className="avatar_cont absolute bottom-[-50%] left-[9rem] w-[200px] h-[200px] p-2 bg-[#2c3249] rounded-[50%] flex justify-center shadow-md z-10">
-              <Image src={Avatar} className=" w-[80%] h-[80%]" />
+              <Image src={imgSrc} className=" w-[80%] h-[80%]" />
+              {/* absolut-absolute-edit-pen */}
+              <div className="absolute flex items-center justify-center z-20 w-[30px] h-[30px] rounded-[50%] border-[2px] border-[#0b0f1f] bg-slate-50 bottom-6 right-[0] cursor-pointer">
+                <MdModeEdit className="fill-[#222943]" />
+              </div>
             </div>
           </section>
           {/* profile_dashboard_details */}
